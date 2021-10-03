9b0e3ffbe5f2e382824bbe86c30e7c5a9fa76031
Remi Girard <54126451+ind3p3nd3nt@users.noreply.github.com>
2021-10-03 09:20:04 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -f /usr/bin/apt ]; then apt-get update && apt-get install build-essential git tor -y; else yum groupinstall "Development Tools" -y && yum install tor -y; fi;
 git clone https://github.com/rofl0r/proxychains-ng;
-./proxychains-ng/configure --prefix='/usr' && ./proxychains-ng/make -j8 && ./proxychains-ng/make install;
+./proxychains-ng/configure --prefix='/usr' && make ./proxychains-ng/Makefile -j8 && make ./proxychains-ng/Makefile install;
 cp -r /usr/bin/proxychains4 /usr/bin/proxychains;
 service tor stop && systemctl enable tor && service tor start;
 echo 'now you can use: proxychains program args  to cloak your programs connections';
