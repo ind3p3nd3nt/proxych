5c5c0747ce444c448fc7dacec35585c0f371ffb8
Remi Girard <54126451+ind3p3nd3nt@users.noreply.github.com>
2021-10-03 09:16:59 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ if [ -f /usr/bin/apt ]; then apt-get update && apt-get install build-essential g
 git clone https://github.com/rofl0r/proxychains-ng;
 ./proxychains-ng/configure --prefix='/usr' && ./proxychains-ng/make -j8 && ./proxychains-ng/make install;
 cp -r /usr/bin/proxychains4 /usr/bin/proxychains;
-service tor stop && systemctl tor enable && service tor start;
-echo 'now you can use: proxychains program args  to cloak your program's connection';
+service tor stop && systemctl enable tor && service tor start;
+echo 'now you can use: proxychains program args  to cloak your programs connections';
